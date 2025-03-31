51c0abc29d9a368f5042ff66c98817c723ca6821
Axel1209 <gauthier.axel@gmail.com>
2025-03-31 09:29:59 +0200

    Update animation.js

--- animation.js
+++ animation.js
@@ -63,6 +63,46 @@ export function displayGoalAnimation() {
     });
 }
 
+// Fonction pour mettre un replay
+export function displayreplay() {
+    return new Promise((resolve) => {
+        const gif = document.createElement("img");
+        // URL corrigée :
+        gif.src = "https://github.com/Axel1209/remontada_bis.github.io/blob/main/sergiroberto.gif";
+        gif.style = `
+            position: fixed;
+            top: 40%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            max-width: 90%;
+            max-height: 90%;
+            z-index: 9999;
+        `;
+        
+        const text = document.createElement("div");
+        text.innerText = "GOAAAAAAAAAAl";
+        text.style = `
+            position: fixed;
+            top: 60%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            font-size: 3em;
+            font-weight: bold;
+            color: red;
+            z-index: 9999;
+        `;
+        
+        document.body.appendChild(gif);
+        document.body.appendChild(text);
+
+        setTimeout(() => {
+            gif.remove();
+            text.remove();
+            resolve(); // Déclenche la suite après 3 secondes
+        }, 3400);
+    });
+}
+
 // Fonction photo cauvin
 export function displayphotocauvin() {
     const existingImage = document.getElementById("topchef-image");
